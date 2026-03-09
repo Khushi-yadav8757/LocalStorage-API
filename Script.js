0c61649f77f4df4ce08a60f4c09c4d7340ad0a53
Khushi Yadav <kky957242@gmail.com>
2026-03-09 14:05:54 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+function saveData(){
+localStorage.setItem("name","Khushi")
+alert("Data Saved in LocalStorage")
+}
+
+function getData(){
+let data = localStorage.getItem("name")
+document.getElementById("output").innerText = data
+}
+
+function removeData(){
+localStorage.removeItem("name")
+alert("Data Removed")
+}
